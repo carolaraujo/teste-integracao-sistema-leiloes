bffd1476add06d2df036dffe8679101db82a316a
Caroline Araujo <ca.araujosouza@gmail.com>
2021-10-12 19:27:16 -0300

    teste de integracao do usuario no banco de dados

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -6,19 +6,26 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
 
 class UsuarioDaoTest {
 	
 	
 	private UsuarioDao dao;
 	
-	private EntityManager em;
 
 	@Test
 	void testBuscaDeUsuarioPeloUserName() {
+		EntityManager em = JPAUtil.getEntityManager();
 		this.dao = new UsuarioDao(em);
-		Usuario usuario = this.dao.buscarPorUsername("fulano");
-		Assert.assertNotNull(usuario);
+		
+		Usuario usuario = new Usuario("fulano", "ulano@email.com", "12345678");
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+		
+		Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
+		Assert.assertNotNull(encontrado);
 	}
 
 }
